4973aaac6a01949df977fa48d9c15e769c30b165
Egor <cupjust@gmail.com>
2022-05-23 10:45:21 +0300

    rotate to vue

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("wavesurfer"),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
